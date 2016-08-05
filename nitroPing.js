fe3faeceaf6d2894cd571fee1f6f076bc8f2cb99
Mike Ralphson <mike.ralphson@gmail.com>
2016-08-05 16:24:08 +0100

    Make nitroPing a little more useful

--- nitroPing.js
+++ nitroPing.js
@@ -6,6 +6,9 @@ var config = require('./config.json');
 var host = config.nitro.host;
 var api_key = config.nitro.api_key;
 
+var start = new Date();
+
 nitro.ping(host,api_key,{},function(obj){
-	console.log(JSON.stringify(obj,null,2));
+	var end = new Date();
+	console.log('1 Packet sent, 1 received. Elapsed time '+(end-start)+'ms');
 });
