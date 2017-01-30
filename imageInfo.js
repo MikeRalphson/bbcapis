a6f31077177a7db929e438457230bff7e95d707b
Mike Ralphson <mike.ralphson@gmail.com>
2017-01-30 13:34:36 +0000

    Add imageInfo.js

---
+++ imageInfo.js
@@ -0,0 +1,33 @@
+var sdk = require('./nitroSdk.js');
+var api = require('./nitroApi/api.js');
+
+var config = require('./config.json');
+
+var query = sdk.newQuery();
+query.add(api.fImagesPid,process.argv[2],true);
+
+var host = config.nitro.host;
+var path = api.nitroImages;
+var key = config.nitro.api_key;
+
+function processResults(obj){
+	console.log(JSON.stringify(obj.nitro.results,null,2));
+}
+
+function handleResponse(obj){
+	processResults(obj);
+	if (obj.nitro.pagination && obj.nitro.pagination.next) {
+		var query = sdk.newQuery();
+		query = query.fromString(obj.nitro.pagination.next.href,true);
+		sdk.make_request(host,path,key,query,{},handleResponse,function(err){
+		});
+	}
+}
+
+if (process.argv.length<3) {
+	console.log('Usage: imageInfo {image-pid}');
+}
+else {
+	sdk.make_request(host,path,key,query,{},handleResponse,function(err){
+	});
+}
