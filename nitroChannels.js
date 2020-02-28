6102da87f10ff2c478c22f1163ed27a1b97ba57a
Mike Ralphson <mike.ralphson@gmail.com>
2020-02-28 14:44:50 +0000

    nitroChannels: clean up output

--- nitroChannels.js
+++ nitroChannels.js
@@ -5,20 +5,18 @@ Lists Nitro masterbrands (channels)
 */
 
 function processResponse(obj) {
-	for (var m in obj.nitro.results.items) {
-		var mb = obj.nitro.results.items[m];
-		//if (mb.partner != 's0000001') {
-		console.log(mb.mid+','+mb.url_key+','+(mb.title ? mb.title : mb.name)+','+mb.partner);
-		//}
-	}
-	var dest = {};
-	if ((obj.nitro.pagination) && (obj.nitro.pagination.next)) {
-		dest.query = nitro.queryFrom(obj.nitro.pagination.next.href,true);
-		dest.path = path;
-		dest.callback = processResponse;
-	}
-	nitro.setReturn(dest);
-	return true;
+  for (var m in obj.nitro.results.items) {
+    var mb = obj.nitro.results.items[m];
+    console.log(mb.mid+','+(mb.url_key||'')+','+(mb.title ? mb.title : (mb.name || ''))+','+mb.partner);
+  }
+  var dest = {};
+  if ((obj.nitro.pagination) && (obj.nitro.pagination.next)) {
+    dest.query = nitro.queryFrom(obj.nitro.pagination.next.href,true);
+    dest.path = path;
+    dest.callback = processResponse;
+  }
+  nitro.setReturn(dest);
+  return true;
 }
 
 var nitro = require('./nitroSdk');
@@ -36,5 +34,5 @@ query.add(api.fMasterbrandsPartnerPid,'*',true);
 nitro.make_request(host,path,api_key,query,{},processResponse);
 
 process.on('exit', function(code) {
-	//console.log('About to exit with code:', code);
+  //console.log('About to exit with code:', code);
 });
